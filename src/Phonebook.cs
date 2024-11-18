ec3d32921002bfc6415eaa73a4a210a11cad3386
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-18 22:36:26 +0000

    Implement

--- src/Phonebook.cs
+++ src/Phonebook.cs
@@ -3,30 +3,42 @@ using System.Collections.Generic;
 
 public class Phonebook
 {
+  private Dictionary<string, string> _contacts = new Dictionary<string, string>();
 
   public void AddContact(string name, string phoneNumber)
   {
-
+    if (!_contacts.ContainsKey(name))
+    {
+      _contacts.Add(name, phoneNumber);
+    }
   }
 
   public string GetPhoneNumber(string name)
   {
-
+    return _contacts.TryGetValue(name, out string phoneNumber) ? phoneNumber : null;
   }
 
   public void UpdateContact(string name, string newPhoneNumber)
   {
-
+    if (_contacts.ContainsKey(name))
+    {
+      _contacts[name] = newPhoneNumber;
+    }
   }
 
   public void RemoveContact(string name)
   {
-
+    _contacts.Remove(name);
   }
 
   public void DisplayAllContacts()
   {
+    foreach (var contact in _contacts)
+    {
+      Console.WriteLine($"{contact.Key}: {contact.Value}");
+    }
 
-
+    // var allNames = _contact.Keys;
+    // var allPhoneNumbers = _contact.Values;
   }
 }
