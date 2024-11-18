8977a462bae109ec6cc6df536e6190cddacd2e13
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-18 22:04:47 +0000

    Implement

--- src/ShoppingCartApp/Services/CartService.cs
+++ src/ShoppingCartApp/Services/CartService.cs
@@ -4,25 +4,55 @@ namespace ShoppingCartApp.Services
 {
   public class CartService
   {
-
+    private readonly List<CartItem> _cart = new();
     public IEnumerable<CartItem> GetAll()
     {
-      return null;
+      return _cart;
     }
 
     public CartItem AddToCart(Product product)
     {
-      return null;
+      var existingItem = _cart.FirstOrDefault(i => i.ProductName == product.Name);
+      if (existingItem != null)
+      {
+        existingItem.Quantity++;
+        return existingItem;
+      }
+      else
+      {
+        var newItem = new CartItem
+        {
+          Id = product.Id,
+          ProductName = product.Name,
+          Price = product.Price,
+          Quantity = 1
+        };
+        _cart.Add(newItem);
+        return newItem;
+      }
     }
 
     public bool RemoveFromCart(int id)
     {
-      return false;
+      var item = _cart.FirstOrDefault(i => i.Id == id);
+      if (item != null)
+      {
+        item.Quantity--;
+        if (item.Quantity == 0)
+        {
+          _cart.Remove(item);
+        }
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public decimal GetCartTotal()
     {
-      return 0;
+      return _cart.Sum(i => i.TotalPrice);
     }
   }
 }
