68bbb4aee7658dc279b64287de931b5765cc7cfc
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-18 22:24:33 +0000

    Set up starter

---
+++ src/Phonebook.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class Phonebook
+{
+
+  public void AddContact(string name, string phoneNumber)
+  {
+
+  }
+
+  public string GetPhoneNumber(string name)
+  {
+
+  }
+
+  public void UpdateContact(string name, string newPhoneNumber)
+  {
+
+  }
+
+  public void RemoveContact(string name)
+  {
+
+  }
+
+  public void DisplayAllContacts()
+  {
+
+
+  }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿class Program
+{
+  static void Main()
+  {
+    Phonebook phonebook = new Phonebook();
+
+    phonebook.AddContact("Alice", "555-1234");
+    phonebook.AddContact("Bob", "555-5678");
+
+    phonebook.DisplayAllContacts();
+
+    Console.WriteLine(phonebook.GetPhoneNumber("Alice"));
+
+    phonebook.UpdateContact("Bob", "555-8765");
+
+    phonebook.RemoveContact("Alice");
+
+    phonebook.DisplayAllContacts();
+  }
+}
