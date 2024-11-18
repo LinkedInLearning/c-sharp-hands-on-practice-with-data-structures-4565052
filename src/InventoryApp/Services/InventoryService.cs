892f25d01b325ab8c84518d1d020d49e48160683
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-18 23:26:09 +0000

    Implement

--- src/InventoryApp/Services/InventoryService.cs
+++ src/InventoryApp/Services/InventoryService.cs
@@ -6,24 +6,44 @@ namespace InventoryApp.Services
   public class InventoryService
   {
 
+    private readonly Dictionary<string, InventoryItem> _inventory = new();
+
     public IEnumerable<InventoryItem> GetAll()
     {
-      return null;
+      return _inventory.Values;
     }
 
     public InventoryItem GetByProductCode(string productCode)
     {
-      return null;
+      _inventory.TryGetValue(productCode, out var item);
+      return item;
     }
 
     public void Add(InventoryItem newItem)
     {
-
+      if (_inventory.TryGetValue(newItem.ProductCode, out var item))
+      {
+        item.Quantity = item.Quantity + newItem.Quantity;
+      }
+      else
+      {
+        _inventory[newItem.ProductCode] = newItem;
+      }
     }
 
     public void RemoveOne(string productCode)
     {
-
+      if (_inventory.TryGetValue(productCode, out var item))
+      {
+        if (item.Quantity > 1)
+        {
+          item.Quantity--;
+        }
+        else
+        {
+          _inventory.Remove(productCode);
+        }
+      }
     }
   }
 }
