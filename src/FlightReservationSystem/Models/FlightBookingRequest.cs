143629fc886314961bd2582ad2976235b400ef92
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-18 17:00:47 +0000

    Update starte

--- src/FlightReservationSystem/Models/FlightBookingRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace FlightReservationSystem.Models
-{
-  public class FlightBookingRequest
-  {
-    public string FlightNumber { get; set; }
-    public string PassengerName { get; set; }
-    public string PassengerEmail { get; set; }
-  }
-}
