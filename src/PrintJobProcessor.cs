1391121973a8fadf9e54ec68c60c305bf3903dc8
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 17:35:01 +0000

    Implemented

--- src/PrintJobProcessor.cs
+++ src/PrintJobProcessor.cs
@@ -4,23 +4,30 @@ using System.Collections.Generic;
 class PrintJobProcessor
 {
 
+  private Queue<string> printQueue = new Queue<string>();
+
   public void AddJob(string documentName)
   {
-
+    printQueue.Enqueue(documentName);
   }
 
   public void ProcessPrintJobs()
   {
-
+    // TryDequeue(out result)
+    while(printQueue.Count > 0) {
+      string documentName = printQueue.Dequeue();
+      Console.WriteLine($"Printing document: {documentName}");
+    }
   }
 
   public string GetNextPrintJob()
   {
-
+    // return printQueue.Count > 0 ? printQueue.Peek() : null;
+    return printQueue.TryPeek(out string document) ? document : null;
   }
 
   public void ClearJobs()
   {
-
+    printQueue.Clear();
   }
 }
