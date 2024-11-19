e5fb3fb676c823803932b92c072c8cffc0a1fdb1
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 22:24:47 +0000

    Implement

--- src/TextEditorApp/Services/TextEditorService.cs
+++ src/TextEditorApp/Services/TextEditorService.cs
@@ -6,20 +6,28 @@ namespace TextEditorApp.Services
     private string _currentText = string.Empty;
 
     public string GetText() => _currentText;
+    private Stack<string> _undoStack = new Stack<string>();
 
     public void SaveText(string newText)
     {
-
+      _undoStack.Push(_currentText);
+      _currentText += newText;
     }
 
     public void ClearText()
     {
-
+      _undoStack.Push(_currentText);
+      _currentText = string.Empty;
     }
 
     public bool Undo()
     {
-
+      if (_undoStack.Count > 0)
+      {
+        _currentText = _undoStack.Pop();
+        return true;
+      }
+      return false;
     }
 
     public bool Redo()
