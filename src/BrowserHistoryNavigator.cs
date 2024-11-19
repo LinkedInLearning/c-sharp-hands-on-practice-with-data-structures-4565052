188e213b0cc42a5cef362520ae27a9416c71bc07
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 20:58:18 +0000

    Implement

--- src/BrowserHistoryNavigator.cs
+++ src/BrowserHistoryNavigator.cs
@@ -2,18 +2,26 @@ using System.Collections.Generic;
 
 class BrowserHistoryNavigator
 {
+  private Stack<string> browserHistory = new Stack<string>();
   public void visitWebsite(string website)
   {
-
+    browserHistory.Push(website);
   }
 
   public void backToPreviousWebsite()
   {
-
+    if (browserHistory.Count > 0)
+    {
+      browserHistory.Pop();
+    }
   }
 
   public string getCurrentWebsite()
   {
-
+    if (browserHistory.Count > 0)
+    {
+      return browserHistory.Peek();
+    }
+    return null;
   }
 }
