cf49bf5e625888579febc845dc8b1eca8bfcca49
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 00:04:39 +0000

    Update starter

---
+++ src/PrintJobProcessor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class PrintJobProcessor
+{
+
+  public void AddJob(string documentName)
+  {
+
+  }
+
+  public void ProcessPrintJobs()
+  {
+
+  }
+
+  public string GetNextPrintJob()
+  {
+
+  }
+
+  public void ClearJobs()
+  {
+
+  }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿class Program
+{
+  static void Main()
+  {
+    PrintJobProcessor processor = new PrintJobProcessor();
+
+    processor.AddJob("Document1.pdf");
+    processor.AddJob("Document2.pdf");
+    processor.AddJob("Document3.pdf");
+
+    processor.ProcessPrintJobs();
+
+    processor.AddJob("Document4.pdf");
+    processor.AddJob("Document5.pdf");
+
+    Console.WriteLine($"Printing document: {processor.GetNextJob()}");
+
+    processor.ClearJobs();
+
+    processor.AddJob("Document6.pdf");
+
+    processor.ProcessPrintJobs();
+  }
+}
