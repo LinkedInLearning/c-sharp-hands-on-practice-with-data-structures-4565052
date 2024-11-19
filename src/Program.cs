268c037e752e463dad541b07479063d56579103c
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 19:05:33 +0000

    Add starter files

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿class Program
+{
+  static void Main()
+  {
+    string rootDirectory = @"C:\Your\Directory\Path";
+    SystemNavigator navigator = new SystemNavigator(rootDirectory);
+
+    navigator.TraverseDirectory();
+  }
+}
---
+++ src/SystemNavigator.cs
@@ -0,0 +1,25 @@
+class SystemNavigator
+{
+
+  public SystemNavigator(string rootDirectory)
+  {
+
+  }
+
+  public void EnterDirectory(string directory)
+  {
+
+  }
+
+  public void ExitDirectory()
+  {
+
+  }
+
+
+  public void TraverseDirectory()
+  {
+
+  }
+
+}
