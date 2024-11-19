0625c268547021674980deef50a7e3bb6d252816
Kathryn Hodge <blondiebytes@gmail.com>
2024-11-19 18:56:19 +0000

    Implemented

--- src/TrainSystem/Services/TrainService.cs
+++ src/TrainSystem/Services/TrainService.cs
@@ -6,34 +6,53 @@ namespace TrainSystem.Services
 {
   public class TrainService
   {
+    private Queue<Train> _trainQueue = new Queue<Train>();
+    private List<Track> _tracks = new List<Track>() { new Track(1),
+      new Track(2), new Track(3)};
     public IEnumerable<Track> GetAllTracks()
     {
-      return null;
+      return _tracks;
     }
 
     public IEnumerable<Train> GetAllTrains()
     {
-      return null;
+      return _trainQueue;
     }
 
     public Train GetNextTrain()
     {
+      if (_trainQueue.Count > 0)
+      {
+        return _trainQueue.Peek();
+      }
       return null;
     }
 
     public void ArriveTrain(Train train)
     {
-
+      _trainQueue.Enqueue(train);
     }
 
     public void AssignNextTrainToTrack()
     {
-
+      var nextTrain = GetNextTrain();
+      if (nextTrain != null)
+      {
+        foreach (var track in _tracks)
+        {
+          if (track.currentTrain == null)
+          {
+            track.currentTrain = nextTrain;
+            _trainQueue.Dequeue();
+            return;
+          }
+        }
+      }
     }
 
     public void DepartTrain(Track track)
     {
-
+      track.currentTrain = null;
     }
   }
 }
