99138251bcfbd8879fa7917d8e8e91b992593e14
Tguerin12 <tmguerin@outlook.com>
2025-01-09 20:16:06 +0000

    Updated 02_04b

--- src/TodoListApp/Services/TodoService.cs
+++ src/TodoListApp/Services/TodoService.cs
@@ -4,25 +4,46 @@ namespace TodoListApp.Services
 {
   public class TodoService
   {
-    private List<TodoItem> _tasks = new List<TodoItem>();
+    private TodoItem[] _tasks = new TodoItem>[10];
+    private int _count = 0;
 
     public IEnumerable<TodoItem> GetAll()
     {
-      return _tasks;
+      return _tasks.Take(_count);
     }
 
     public void Add(TodoItem todoItem)
     {
-      todoItem.Id = _tasks.Count;
-      _tasks.Add(todoItem);
+      if (_count == _tasks.Length)
+      {
+        ResizeArray ();
+      }
+      todoItem.Id = _count;
+      _tasks[_count] = todoItem;
+      _count++;
     }
 
     public void Delete(int id)
     {
-      var item = _tasks.FirstOrDefault(t => t.Id == id);
-      if (item != null) {
-        _tasks.Remove(item);
+      for (int i = 0; i < _count; i++)
+      {
+        if (_tasks[i].Id == id)
+        {
+          for (int j = i; j < _count - 1; j++)
+          {
+            _tasks[j] = _tasks[j + 1];
+          }
+          _tasks[_count - 1] = null;
+          _count--;
+          break;
+        }
       }
     }
+         
+    private void ResizeArray() {
+      var newArray = new TodoItem [_tasks.Length * 2];
+      Array.Copy(_tasks, newArray, _tasks.Length);
+      _tasks = newArray;
+    }
   }
-}
+}  
